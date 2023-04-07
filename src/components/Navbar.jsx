4e3dc1bdb3fee2ca8e692929c2e2f7583fada904
Ali Raza <ialidar2001@gmail.com>
2023-04-07 07:21:56 +0500

    Done with the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {logo,menu,close} from '../assets';
 
 const Navbar = () => {
   const [active, setactive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
     className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -21,10 +22,46 @@ const Navbar = () => {
         }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Ali Raza <span className='sm:block hidden'>|Software Engineer</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Ali Raza <span className='sm:block hidden'>| Software Engineer</span></p>
         </Link>
 
-        {/* Now here we will add more links for the navbar */}
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li className={`${active===link.title? "text-white":"text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}>
+              <a href={`#${link.id}`}
+              onClick={() => {setactive(link.title)}}
+              >
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        {/* Designing a Hamburger Button */}
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=>setToggle(!toggle)}/>
+
+          <div className={`${!toggle ? 'hidden':'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+
+          <ul className="list-none flex justify-end items-start flex-col gap-4">
+          {navLinks.map((link) => (
+            <li className={`${active===link.title? "text-white":"text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`}>
+              <a href={`#${link.id}`}
+              onClick={() => {
+                setToggle(!toggle);
+                setactive(link.title);
+              
+              }}
+              >
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+          </div>
+        </div>
 
       </div>
 
