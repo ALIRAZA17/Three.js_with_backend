23c60afecffda13c5e6d1b858170ac9fdaab9f5a
Ali Raza <ialidar2001@gmail.com>
2023-04-11 10:15:40 +0500

    Working to extend the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 
       <p className={`${styles.heroSubText} mt-2 text-white-100`}>
         I develop 3D visuals, user <br className='sm:block hidden'/>
-        interfaces and web applications.
+        interfaces, web and mobile applications.
       </p>
         </div>
       </div>
