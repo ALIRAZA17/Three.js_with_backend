4fddb12687cba35626255ca4904b67bd586e3fc1
Ali Raza <ialidar2001@gmail.com>
2023-05-17 05:33:35 +0500

    Fetching navlinks from the backend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 import { styles } from '../styles'
-import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
